ce4aac5c23517f596b97109496a8f2da934ae165
Ralph-19 <rdlm815@gmail.com>
2022-08-03 18:03:22 +0800

    added pages

---
+++ src/app/pgroomer/pgroomer.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pgroomer',
+  templateUrl: './pgroomer.page.html',
+  styleUrls: ['./pgroomer.page.scss'],
+})
+export class PgroomerPage implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
